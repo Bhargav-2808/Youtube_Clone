ae4662ae97340ab190c96e302a82063ac620cb56
Bhargav-2808 <bhargavvalani01@gmail.com>
2022-01-01 18:45:59 +0530

    headerr updated

--- youtubeclone/src/App.js
+++ youtubeclone/src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
-import react , {usestate} from 'react'
+import "./App.scss";
+import react , {useState} from 'react'
+import  {Header}  from "./Components/Header/Header";
 import { Sidebar } from "./Components/SIdebar/Sidebar";
 import LoginScreen from "./Screen/LoginScreen/LoginScreen";
 
@@ -8,8 +9,12 @@ function App() {
   const handleToggleSidebar = () => toggleSidebar(value => !value)
   return (
     <>
+      <div className="app_">
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      
       {/* <LoginScreen /> */}
       <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+      </div>
     </>
   );
 }
--- youtubeclone/src/Components/Header/Header.js
+++ youtubeclone/src/Components/Header/Header.js
@@ -1,7 +1,47 @@
 import './Header.scss';
-export const Header = ()=> {
+import {useForm}from'react-hook-form'
+import HorizontalSplitIcon from '@mui/icons-material/HorizontalSplit';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AppsIcon from '@mui/icons-material/Apps';
+
+export const Header = ({ handleToggleSidebar })=> {
+    const {register ,handleSubmit} = useForm();
+    const onSubmit = data => console.log(data);
+    
+    
     return(
+
         <>
+        <div className="header ">
+         <HorizontalSplitIcon
+            className="header__menu"
+            
+            onClick={() => handleToggleSidebar()}
+         />
+
+         <img
+            src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+            alt=""
+            className="header__logo"
+         />
+
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Search" name="search" {...register("search")} />
+            <button type="submit">
+               <SearchIcon />
+            </button>
+         </form>
+
+         <dic className="header__icons">
+            <NotificationsIcon />
+            <AppsIcon  />
+            <img
+               src="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+               alt="avatar"
+            />
+         </dic>
+      </div>
         </>
     )
   };
